2952c0f6c3299291fe39520770c7e9c5d7d9bd51
HwangHoChan <ghckss93@gmail.com>
2019-10-26 14:25:06 +0900

    list 컴포넌트 구성

--- src/components/todo/list/index.js
+++ src/components/todo/list/index.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { Button } from "widgets";
+import "./index.scss";
 
 const List = () => {
-  return <div>Todo List Component</div>;
+  return (
+    <div className="list">
+      <li>
+        <span className="todo-value">todo1</span>
+        <Button value="삭제" />
+        <Button value="수정" />
+      </li>
+      <li>
+        <span className="todo-value">
+          todo2 todo2 todo2 todo2 todo2 todo2 todo2 todo2 todo2
+        </span>
+        <Button value="삭제" />
+        <Button value="수정" />
+      </li>
+    </div>
+  );
 };
 
 export default List;
